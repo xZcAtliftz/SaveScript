675d0006b7618e0b672d7596af11145d7fb124f4
ErenYeaker <99465722+ErenYeaker@users.noreply.github.com>
2022-02-22 23:50:21 +0700

    Update Attack-Save.lua

--- Attack-Save.lua
+++ Attack-Save.lua
@@ -7292,6 +7292,10 @@ Fire.Size = 2
 
 end)
 
+Tab8:Button("Korblox",function()
+	local ply = game.Players.LocalPlayer local chr = ply.Character chr.RightLowerLeg.MeshId = "902942093" chr.RightLowerLeg.Transparency = "1" chr.RightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096" chr.RightUpperLeg.TextureID = "http://roblox.com/asset/?id=902843398" chr.RightFoot.MeshId = "902942089" chr.RightFoot.Transparency = "1"
+end)
+
 Tab8:Toggle("Xray",false, function(t)
 	if t then
 		for i,v in pairs(workspace:GetDescendants()) do
